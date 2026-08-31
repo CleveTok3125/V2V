94d294292e6fad26b06c4b5d91ec5a34090987c1
CleveTok3125 <CleveTok3125@gmail.com>
2026-08-31 16:48:42 +0700

    build: replace 5 shell scripts with Makefile (parallel, trimpath, vet/test)
    - Merge build_client.sh/build_ctl.sh 95% duplicate matrix into shared PLATFORMS loop (7 targets, V2V-/V2Vctl-, CGO_ENABLED=0, -trimpath, Version stamping)
    - Add server/web/vet/test/clean/help targets; make -j4 parallel and CI vet+test before build
    - Delete build.sh/build_*.sh, update Dockerfile and release.yml to use make

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-
-./build_server.sh
-./build_client.sh
-./build_web.sh
-./build_ctl.sh
--- build_server.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-
-mkdir -p public
-go build -tags netgo -ldflags '-s -w' -o ./public/server.bin ./server
-sh ./build_web.sh
